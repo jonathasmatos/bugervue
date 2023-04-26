e5b390a350c82629d1a63555021396322739bb97
jonathasmatos <jonathasvirtual@gmail.com>
2023-04-25 21:43:19 -0300

    comunicação com back-end-fake

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger</h1>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    Navbar,
+    Banner,
+    BurgerForm
+  }
+}
+</script>
---
+++ src/views/Pedidos.vue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-container">
+    <h1>PEDIDOS</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PedidosView",
+};
+</script>
+<style>
+</style>
